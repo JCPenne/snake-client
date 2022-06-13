6c057cb93c5027358ad0878ae66238667a694f64
JCPenne <jackcpalmer@icloud.com>
2022-06-13 14:34:28 -0700

    add key listener with ctrl-c process.exit function call

--- play.js
+++ play.js
@@ -1,1 +1,20 @@
 const { connect } = require('./client');
+
+const handleUserInput = () => {
+  if ('\u0003') {
+    process.exit();
+  }
+};
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  stdin.on('data', handleUserInput);
+
+  return stdin;
+};
+setupInput();
+
+module.exports = setupInput;
